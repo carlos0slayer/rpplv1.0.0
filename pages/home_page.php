7dd561ed55b01f49ad4d01165f6805d673d10641
Carlosytere123 <carlos0slayer@gmail.com>
2022-04-01 13:20:19 -0600

    asdsad

---
+++ pages/home_page.php
@@ -0,0 +1,167 @@
+<div class="col-md-8 ml-auto mr-auto">
+    <h2 class="text-center">Consulta de expedientes</h2>
+</div>
+<div class="row mt-5">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="toolbar">
+                </div>
+                <table id="datatable" class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>folio</th>
+                            <th>nombres</th>
+                            <th>paterno</th>
+                            <th>materno</th>
+                            <th>nacimiento</th>
+                            <th>originario</th>
+                            <th>telefonos</th>
+                            <th>calle</th>
+                            <th>ext</th>
+                            <th>int</th>
+                            <th>colonia</th>
+                            <th>cp</th>
+                            <th>estado</th>
+                            <th>mucipio</th>
+                            <th>Entre calle 1</th>
+                            <th>Entre calle 2</th>
+                            <th>Latitud</th>
+                            <th>Longitud</th>
+                            <th>marca</th>
+                            <th>submarca</th>
+                            <th>placa</th>
+                            <th>modelo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>MOR123123</td>
+                            <td>Carlos</td>
+                            <td>Jimenez</td>
+                            <td>Avalos</td>
+                            <td>08/12/1995</td>
+                            <td>Morelia</td>
+                            <td>4521996106</td>
+                            <td>1a Cerrada Jose Antonio Cisneros</td>
+                            <td>50</td>
+                            <td>16</td>
+                            <td>Torreon Nuevo</td>
+                            <td>58116</td>
+                            <td>Michoacan</td>
+                            <td>Morelia</td>
+                            <td>Dramaturgo Mexicano</td>
+                            <td>2da Cerra Jose Antonio Cisneros</td>
+                            <td>19.123456</td>
+                            <td>-101.123456</td>
+                            <td>BMW</td>
+                            <td>M8</td>
+                            <td>PGJ-AS1-12</td>
+                            <td>2022</td>
+                        </tr>
+                        <tr>
+                            <td>MOR123123</td>
+                            <td>Carlos</td>
+                            <td>Jimenez</td>
+                            <td>Avalos</td>
+                            <td>08/12/1995</td>
+                            <td>Morelia</td>
+                            <td>4521996106</td>
+                            <td>1a Cerrada Jose Antonio Cisneros</td>
+                            <td>50</td>
+                            <td>16</td>
+                            <td>Torreon Nuevo</td>
+                            <td>58116</td>
+                            <td>Michoacan</td>
+                            <td>Morelia</td>
+                            <td>Dramaturgo Mexicano</td>
+                            <td>2da Cerra Jose Antonio Cisneros</td>
+                            <td>19.123456</td>
+                            <td>-101.123456</td>
+                            <td>BMW</td>
+                            <td>M8</td>
+                            <td>PGJ-AS1-12</td>
+                            <td>2022</td>
+                        </tr>
+                        <tr>
+                            <td>MOR123123</td>
+                            <td>Carlos</td>
+                            <td>Jimenez</td>
+                            <td>Avalos</td>
+                            <td>08/12/1995</td>
+                            <td>Morelia</td>
+                            <td>4521996106</td>
+                            <td>1a Cerrada Jose Antonio Cisneros</td>
+                            <td>50</td>
+                            <td>16</td>
+                            <td>Torreon Nuevo</td>
+                            <td>58116</td>
+                            <td>Michoacan</td>
+                            <td>Morelia</td>
+                            <td>Dramaturgo Mexicano</td>
+                            <td>2da Cerra Jose Antonio Cisneros</td>
+                            <td>19.123456</td>
+                            <td>-101.123456</td>
+                            <td>BMW</td>
+                            <td>M8</td>
+                            <td>PGJ-AS1-12</td>
+                            <td>2022</td>
+                        </tr>
+                        <tr>
+                            <td>MOR123123</td>
+                            <td>Carlos</td>
+                            <td>Jimenez</td>
+                            <td>Avalos</td>
+                            <td>08/12/1995</td>
+                            <td>Morelia</td>
+                            <td>4521996106</td>
+                            <td>1a Cerrada Jose Antonio Cisneros</td>
+                            <td>50</td>
+                            <td>16</td>
+                            <td>Torreon Nuevo</td>
+                            <td>58116</td>
+                            <td>Michoacan</td>
+                            <td>Morelia</td>
+                            <td>Dramaturgo Mexicano</td>
+                            <td>2da Cerra Jose Antonio Cisneros</td>
+                            <td>19.123456</td>
+                            <td>-101.123456</td>
+                            <td>BMW</td>
+                            <td>M8</td>
+                            <td>PGJ-AS1-12</td>
+                            <td>2022</td>
+                        </tr>
+                        <tr>
+                            <td>MOR123123</td>
+                            <td>Carlos</td>
+                            <td>Jimenez</td>
+                            <td>Avalos</td>
+                            <td>08/12/1995</td>
+                            <td>Morelia</td>
+                            <td>4521996106</td>
+                            <td>1a Cerrada Jose Antonio Cisneros</td>
+                            <td>50</td>
+                            <td>16</td>
+                            <td>Torreon Nuevo</td>
+                            <td>58116</td>
+                            <td>Michoacan</td>
+                            <td>Morelia</td>
+                            <td>Dramaturgo Mexicano</td>
+                            <td>2da Cerra Jose Antonio Cisneros</td>
+                            <td>19.123456</td>
+                            <td>-101.123456</td>
+                            <td>BMW</td>
+                            <td>M8</td>
+                            <td>PGJ-AS1-12</td>
+                            <td>2022</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!-- end content-->
+        </div>
+        <!--  end card  -->
+    </div>
+    <!-- end col-md-12 -->
+</div>
+<!-- end row -->
